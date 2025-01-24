761b2aedeb1f9a5304f947332142066656baa40f
masudulalam <masuudulalam@gmail.com>
2025-01-25 03:25:41 +0600

    added react project basic setup includes react router, tailwind css and daisyUI with vite

---
+++ src/components/root/Root.jsx
@@ -0,0 +1,11 @@
+
+
+const Root = () => {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold underline text-green-500 text-center bg-gray-200 p-3 rounded-lg">Hello world!</h1>
+        </div>
+    );
+};
+
+export default Root;
