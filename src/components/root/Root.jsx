e7bce1eceb1c9f124a82a59b8c6d9ef5817f8e06
masudulalam <masuudulalam@gmail.com>
2025-01-25 04:42:45 +0600

    added navbar and footer

--- src/components/root/Root.jsx
+++ src/components/root/Root.jsx
@@ -1,9 +1,14 @@
+import { Outlet } from "react-router-dom";
+import Footer from "../footer/Footer";
+import Navbar from "../navbar/Navbar";
 
 
 const Root = () => {
     return (
         <div>
-            <h1 className="text-3xl font-bold underline text-green-500 text-center bg-gray-200 p-3 rounded-lg">Hello world!</h1>
+            <Navbar />
+            <Outlet />
+            <Footer />
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,29 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import Root from './components/root/Root';
+import Home from './components/home/Home';
+import Statistics from './components/statistics/Statistics';
+import Dashboard from './components/dashboard/Dashboard';
 
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "/statistics",
+        element: <Statistics />
+      },
+      {
+        path: "/dashboard",
+        element: <Dashboard />
+      }
+    ]
   },
 ]);
 
