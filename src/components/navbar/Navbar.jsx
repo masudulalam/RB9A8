2e13236c2c35040721be1c71c7afb75b7b05ef81
masudulalam <masuudulalam@gmail.com>
2025-01-25 05:02:27 +0600

    added home, statistics, and dashboard route

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,4 +1,14 @@
+import { NavLink } from "react-router-dom";
+
+
 const Navbar = () => {
+
+  const links = <>
+    <li><NavLink to="/">Home</NavLink></li>
+    <li><NavLink to="/statistics">Statistics</NavLink></li>
+    <li><NavLink to="/dashboard">Dashboard</NavLink></li>
+    
+  </>
   return (
     <div className="navbar bg-base-100 shadow-sm">
       <div className="navbar-start">
@@ -23,24 +33,14 @@ const Navbar = () => {
             tabIndex={0}
             class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
           >
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
+            {links}
           </ul>
         </div>
-        <a className="btn btn-ghost text-xl">daisyUI</a>
+        <a className="btn btn-ghost text-xl">Gadget Heaven</a>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">
-          <li>
-            <a>Item 1</a>
-          </li>
-          <li>
-            <a>Item 3</a>
-          </li>
+            {links}
         </ul>
       </div>
       <div className="navbar-end">
